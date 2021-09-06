ef3f77a4ad4233071cdad9bb99b9ea335d6793b1
Conor Nugent <nugentc@gmail.com>
2021-09-06 11:25:37 +0100

    capture conversation state

--- gradio_demo.py
+++ gradio_demo.py
@@ -9,12 +9,14 @@ import gradio as gr
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+text_session= ''
 
 def dialogpt(text):
     # encode the new user input, add the eos_token and return a tensor in Pytorch
     for step in range(50000):
-
-        new_user_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
+        global text_session
+        text_session += '\n '+text
+        new_user_input_ids = tokenizer.encode(text_session + tokenizer.eos_token, return_tensors='pt')
 
         # append the new user input tokens to the chat history
         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
@@ -23,7 +25,9 @@ def dialogpt(text):
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
         # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        output =  tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        text_session += '\n '+output
+        return text_session
 
 inputs = gr.inputs.Textbox(lines=1, label="Input Text")
 outputs =  gr.outputs.Textbox(label="DialoGPT")
