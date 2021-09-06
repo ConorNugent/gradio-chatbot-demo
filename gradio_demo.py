3aad3458e15440b5d6c7c3407866911b10bd5eed
Conor Nugent <nugentc@gmail.com>
2021-09-06 15:20:31 +0100

    tidy up commented out code

--- gradio_demo.py
+++ gradio_demo.py
@@ -19,8 +19,6 @@ def dialogpt(text):
      # encode the new user input, add the eos_token and return a tensor in Pytorch
     global chat_history_ids
     text_session.append(text)
-    # text_input = tokenizer.eos_token.join(text_session)
-    # text_session += text+tokenizer.eos_token
     new_user_input_ids = tokenizer.encode(text+tokenizer.eos_token, return_tensors='pt')
     # append the new user input tokens to the chat history
     bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if chat_history_ids is not None else new_user_input_ids
@@ -28,10 +26,6 @@ def dialogpt(text):
     # generated a response while limiting the total chat history to 1000 tokens, 
     chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
     print("The text is ", [text])
-    # tokenized_phrases = grammar_tokenizer([text], return_tensors='pt', padding=True)
-    # corrections = grammar_model.generate(**tokenized_phrases)
-    # corrections = grammar_tokenizer.batch_decode(corrections, skip_special_tokens=True)
-    # print("The corrections are: ", corrections)
 
     # pretty print last ouput tokens from bot
     output =  tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
