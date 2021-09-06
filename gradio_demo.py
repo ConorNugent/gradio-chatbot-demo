1e64bcc719013e78ef4b0c1b5e9bc1e584eeb601
Conor Nugent <nugentc@gmail.com>
2021-09-06 15:00:59 +0100

    add some some placeholder feedback

--- gradio_demo.py
+++ gradio_demo.py
@@ -3,41 +3,65 @@
 # Please contact the creator for any potential issue. 
 
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 import gradio as gr
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
-text_session= ''
+grammar_tokenizer = AutoTokenizer.from_pretrained("prithivida/grammar_error_correcter_v1")
+grammar_model = AutoModelForSeq2SeqLM.from_pretrained("prithivida/grammar_error_correcter_v1")
+
+text_session = []
+chat_history_ids = None
 
 def dialogpt(text):
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-    for step in range(50000):
-        global text_session
-        text_session += '\n '+text
-        new_user_input_ids = tokenizer.encode(text_session + tokenizer.eos_token, return_tensors='pt')
+     # encode the new user input, add the eos_token and return a tensor in Pytorch
+    global chat_history_ids
+    text_session.append(text)
+    # text_input = tokenizer.eos_token.join(text_session)
+    # text_session += text+tokenizer.eos_token
+    new_user_input_ids = tokenizer.encode(text+tokenizer.eos_token, return_tensors='pt')
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if chat_history_ids is not None else new_user_input_ids
 
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+    # generated a response while limiting the total chat history to 1000 tokens, 
+    chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
+    print("The text is ", [text])
+    # tokenized_phrases = grammar_tokenizer([text], return_tensors='pt', padding=True)
+    # corrections = grammar_model.generate(**tokenized_phrases)
+    # corrections = grammar_tokenizer.batch_decode(corrections, skip_special_tokens=True)
+    # print("The corrections are: ", corrections)
 
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    # pretty print last ouput tokens from bot
+    output =  tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    print("The outout is :", output)
+    text_session.append(output)
+    return '\n'.join(text_session)+'\n\n'+feedback(text)
 
-        # pretty print last ouput tokens from bot
-        output =  tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        text_session += '\n '+output
-        return text_session
+
+def feedback(text):
+    tokenized_phrases = grammar_tokenizer([text], return_tensors='pt', padding=True)
+    corrections = grammar_model.generate(**tokenized_phrases)
+    corrections = grammar_tokenizer.batch_decode(corrections, skip_special_tokens=True)
+    print("The corrections are: ", corrections)
+    if corrections[0] == text:
+        feedback = f'Looks good! Keep up the good work'
+    else:
+        feedback = f'\'{corrections[0]}\' might be a little better'
+    return f'FEEDBACK:  {feedback}'
+
 
 inputs = gr.inputs.Textbox(lines=1, label="Input Text")
-outputs =  gr.outputs.Textbox(label="DialoGPT")
+outputs =  [gr.outputs.Textbox(label="Conversation")]
 
-title = "DialoGPT"
-description = "demo for Microsoft DialoGPT with Hugging Face transformers. To use it, simply input text or click one of the examples text to load them. Read more at the links below. *This is not a Microsoft product and is developed for Gradio*"
-article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1911.00536'>DialoGPT: Large-Scale Generative Pre-training for Conversational Response Generation</a> | <a href='https://github.com/microsoft/DialoGPT'>Github Repo</a> | <a href='https://huggingface.co/microsoft/DialoGPT-large'>Hugging Face DialoGPT-large</a></p>"
+title = "A Conversational Agent for Language Learning"
+description = "A quick roof of concepy using Gradio and a possible starting point for the hackathon"
+article = "<p style='text-align: center'><a href='https://docs.google.com/presentation/d/11fiO91MKZVgNoQJh5pn3Tw8-inHe6XbWYB2r1f701WI/edit?usp=sharing'> A conversational agent for Language learning</a> | <a href='https://github.com/ConorNugent/gradio-chatbot-demo'>Github Repo</a></p>"
 examples = [
             ["Hi, how are you?"],
             ["How far away is the moon?"],
 ]
 
-gr.Interface(dialogpt, inputs, outputs, title=title, description=description, article=article, examples=examples).launch(debug=True)
+
+gr.Interface([dialogpt], inputs, outputs, title=title, description=description, article=article, examples=examples).launch(debug=True)
